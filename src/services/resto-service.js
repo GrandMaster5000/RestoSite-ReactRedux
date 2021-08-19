e4f73e15ca95a1f2ceb33f1a37779408e00de733
Tolik22869 <ggamer556@mail.ru>
2021-08-19 13:27:15 +0300

    work with server , load food card

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,18 @@
 export default class RestoService {
-    getMenuItems() {
-        return [];
+    _apiBase = 'http://localhost:3000';
+
+     async getResource(url) {
+         const res = await fetch(`${this._apiBase}${url}`);
+
+         if(!res.ok) {
+             throw new Error (`Could not fetch ${this._apiBase}${url}`)
+         }
+
+         return await res.json();
+     }
+
+     getMenuItems = async () => {
+        return await this.getResource('/menu/');
     }
+
 }
