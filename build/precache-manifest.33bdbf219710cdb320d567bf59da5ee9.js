cd01c677a6c924de29d51e6bc63f16da02d2e910
Anatoliy Korobskih <82397418+tolik22869@users.noreply.github.com>
2021-08-21 09:57:34 +0300

    add build package

---
+++ build/precache-manifest.33bdbf219710cdb320d567bf59da5ee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7f30e9d6b47649f685350bcb1946f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ae6c5bf40f455c9f5bf",
+    "url": "/static/css/main.6f205f76.chunk.css"
+  },
+  {
+    "revision": "e11f21077404ddcb3053",
+    "url": "/static/js/2.961e0ea2.chunk.js"
+  },
+  {
+    "revision": "8ae6c5bf40f455c9f5bf",
+    "url": "/static/js/main.e4942b36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
